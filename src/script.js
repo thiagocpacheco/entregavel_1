8386056411f932735058fd66e639635d2241b7c1
Thiago Pacheco <thiago2003.cp@gmail.com>
2023-12-14 10:58:07 -0300

    Adicionando a função calculadora ao código

--- src/script.js
+++ src/script.js
@@ -1,11 +1,35 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function calculadora(numero1, numero2, operador) {
+  let resultado;
+  let num1 = parseInt(numero1);
+  let num2 = parseInt(numero2);
 
-//ESCREVA SEU CÓDIGO AQUI!!
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2;
+      break;
+    case "-":
+      resultado = num1 - num2;
+      break;
+    case "*":
+      resultado = num1 * num2;
+      break;
+    case "/":
+      resultado = num1 / num2;
+      break;
+    case "e":
+      let contador = 1;
+      resultado = num1 * num1;
+      while (contador < num2) {
+        resultado = resultado * num1;
+        contador++;
+      }
+      break;
+  }
+  if (resultado == undefined || resultado > 1000000) {
+    resultado = "ERRO";
+  }
+  return resultado;
+}
 
 let visor = document.getElementById("visor");
 
@@ -18,13 +42,12 @@ function limpezatotal() {
 }
 
 function calcular() {
-  let numeros = visor.value.split(/[^0-9]/)
-  let operadores = visor.value.split(/[0-9]/)
-  let operador
-  operadores.forEach(x => {
-    if (x != "")
-      operador = x
+  let numeros = visor.value.split(/[^0-9]/);
+  let operadores = visor.value.split(/[0-9]/);
+  let operador;
+  operadores.forEach((x) => {
+    if (x != "") operador = x;
   });
 
-  visor.value = Calculadora(numeros[0], numeros[1], operador)
+  visor.value = calculadora(numeros[0], numeros[1], operador);
 }
